73790d4c204138e9c432f2ff2d2aef40a815058c
David Ryan <davidryan1998@hotmail.com>
2019-04-26 10:01:36 +0700

    Setting up audio area to play notes

--- src/components/NoteTableRowC.js
+++ src/components/NoteTableRowC.js
@@ -2,18 +2,23 @@ import { connect } from 'react-redux'
 
 import NoteTableRow from './NoteTableRow'
 
-import { playNote, pauseNote, dupNote, delNote, moveDown } from '../actions'
+import { playNote as actionPlayNote, pauseNote, dupNote, delNote, moveDown } from '../actions'
+
+import { playNote as audioPlayNote } from '../audio/playNote'
 
 const mapStateToProps = (state, ownProps) => ({
 })
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  playNote: () => dispatch(playNote({idx: ownProps.idx})),
-  pauseNote: () => dispatch(pauseNote({idx: ownProps.idx})),
   dupNote: () => dispatch(dupNote({idx: ownProps.idx})),
   delNote: () => dispatch(delNote({idx: ownProps.idx})),
   moveDown: () => dispatch(moveDown({idx: ownProps.idx})),
-  moveUp: () => dispatch(moveDown({idx: ownProps.idx - 1}))
+  moveUp: () => dispatch(moveDown({idx: ownProps.idx - 1})),
+  playNote: () => {
+    audioPlayNote(ownProps.row)
+    return dispatch(actionPlayNote({idx: ownProps.idx}))
+  },
+  pauseNote: () => dispatch(pauseNote({idx: ownProps.idx}))
 })
 
 export default connect(
